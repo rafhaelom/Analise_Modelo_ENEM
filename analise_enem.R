5bc49f2de223836dd9b5fccad4dbaf681b40e401
rafhaelom <rafha-1012@hotmail.com>
2021-09-11 23:32:02 -0300

    :sparkles: retry reading xlsx

--- analise_enem.R
+++ analise_enem.R
@@ -1,11 +1,19 @@
 # Importando Bibliotecas.
 library(usethis)
 library(readr)
+library(readxl)
 
 # Leitura de dados.
+dados_raw_1 <- read_excel("dados/ENEM_2017_2019_AMOSTRA_01.xlsx", sheet = 1)
+dados_raw_12 <- read_excel("dados/ENEM_2017_2019_AMOSTRA_01.xlsx", sheet = 1, n_max = 1000)
+dados_raw_13 <- read_excel("dados/ENEM_2017_2019_AMOSTRA_01.xlsx", sheet = 1, skip = 45001)
+
+
 df <- read.csv("dados/MICRODADOS_ENEM_2018.csv", header = TRUE, 
                sep = ";", nrows = 300000)
-View(df)
+
+
+View(dados_raw_1)
 View(head(df))
 View(tail(df))
 
